79453b7c82333757ef0fa5c817664ab39ce96d2b
Ivan Malenko <ivan.malenko18@gmail.com>
2023-05-27 21:00:21 +0300

    add fetch by ID

--- controllers/shops-controller.js
+++ controllers/shops-controller.js
@@ -6,6 +6,16 @@ const fetchAllShops = async (req, res, next) => {
   res.json(allShops);
 };
 
+const fetchShopById = async (req, res) => {
+  const id = req.params.shopId;
+  const shopById = await Shop.findById(id);
+  if (!shopById) {
+    throw HttpError(404);
+  }
+  res.json(shopById);
+};
+
 module.exports = {
   fetchAllShops: ctrlWrapper(fetchAllShops),
+  fetchShopById: ctrlWrapper(fetchShopById),
 };
--- routes/shops.js
+++ routes/shops.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { fetchAllShops } = require("../controllers/shops-controller");
+const {
+  fetchAllShops,
+  fetchShopById,
+} = require("../controllers/shops-controller");
 
 router.get("/", fetchAllShops);
+router.get("/:shopId", fetchShopById);
 
 module.exports = router;
